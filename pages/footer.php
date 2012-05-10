e1e558a51d66b6e0520d022876f9889562e0ded9
Tho Chevalier <theo.chevalier11@gmail.com>
2012-05-11 01:53:07 +0200

    Twitter account link

--- pages/footer.php
+++ pages/footer.php
@@ -43,7 +43,7 @@
         echo '<div class="tweet_date"><a href="http://twitter.com/'.$user.'/status/'.$oStatus->id.'" target="_blank">'.$date.'</a></div></div>'."\n";
       }
       ?>
-      <li><a href="" target="_blank">lire la suite ...</a></li>
+      <li><a href="https://twitter.com/#!/t_chevalier" target="_blank">lire la suite ...</a></li>
       </ul>
       <ul>
         <li class="footer_title"><?=$langage_index['pub'][$lang]?></li>
