b7e608a7984e4d67b2d375fc49874452a5d15f1e
TheoChevalier <theo.chevalier11@gmail.com>
2012-08-09 18:49:28 +0200

    Typo

--- locales/profil.php
+++ locales/profil.php
@@ -12,19 +12,19 @@ $langage = array(
 'remo' => array(
 	'fr' => "Je représente Mozilla pour la France. À ce titre je communique sur la mission de Mozilla,
           j'assiste à des évènements, j'écris des patchs pour Firefox, et traduit les logiciels Mozilla en français.",
-	'en' => "I represent Mozilla. I communicate about Mozilla mission, attend to events, wrote patches for Firefox,
+	'en' => "I represent Mozilla. I communicate about Mozilla mission, attend to events, write patches for Firefox,
           and translate Mozilla softwares in French."),
 'blog_title' => array(
 	'fr' => "Blogueur",
 	'en' => "Blogger"),
 'blog' => array(
 	'fr' => "Je rédige des billets concernant Mozilla et le Web en général.",
-	'en' => "I mostly wrote articles about Mozilla and the Web."),
+	'en' => "I mostly write articles about Mozilla and the Web."),
 'school_title' => array(
 	'fr' => "Étudiant développeur",
 	'en' => "Student developer"),
 'school' => array(
 	'fr' => "Je suis actuellement en BTS Services Informatiques aux Organisations à Carcassonne. Consultez mon CV !",
-	'en' => "I currently pass an Associate Degree IT Services for Organizations in the south of France. Check my resume!")
+	'en' => "I currently spend an Associate Degree IT Services for Organizations in the south of France. Check my resume!")
 );
  ?>
